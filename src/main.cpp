bcdb6c8ad7d52f899dc6212b46b267d4295efe0d
Giovane <gsvolpato@gmail.com>
2025-11-09 12:35:07 -0300

    Adjust board config and block until serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,23 @@
 #include <Arduino.h>
 #include "gpios.h"
 
+int previousBootButtonState = HIGH;
+
 void setup() {
     Serial.begin(115200);
+    while (!Serial) {
+        delay(10);
+    }
     gpios_setup();
 }
 
 void loop() {
+    int currentBootButtonState = digitalRead(BOOT_BUTTON_PIN);
 
+    if (currentBootButtonState == LOW && previousBootButtonState == HIGH) {
+        Serial.println("Hello, world!");
+    }
+
+    previousBootButtonState = currentBootButtonState;
+    delay(5);
 }
